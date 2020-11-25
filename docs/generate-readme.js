5d6c34b0905966eb9ebe394bcb19453779e4ba98
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-11-24 19:21:29 -0500

    Add autogenerate readme function

--- docs/generate-readme.js
+++ docs/generate-readme.js
@@ -1,6 +1,6 @@
-const components = './package/classes/components'
-const elements = './package/classes/elements'
-const layout = './package/classes/layout'
+const components = '../package/classes/components'
+const elements = '../package/classes/elements'
+const layout = '../package/classes/layout'
 
 const fs = require('fs')
 
@@ -13,11 +13,13 @@ const Hr = `---`
 const Br = `\r\n`
 const A = (text, href) => `[${text}](${href})`
 
-// Programmatically generate a readme file that documents the source code
+// Programmatically document the library source code and generate a README.md file
+
+const READEME_FILENAME = 'readme.md'
 
 // Header
 fs.appendFile(
-  'generated.md',
+  READEME_FILENAME,
   String.prototype.concat(H1(`Semantic Styles`), Br, Br),
   function (err) {
     if (err) throw err
@@ -25,7 +27,7 @@ fs.appendFile(
 )
 
 fs.appendFile(
-  'generated.md',
+  READEME_FILENAME,
   String.prototype.concat(H2(`Responsive, themed, UI design system`), Br, Br),
   function (err) {
     if (err) throw err
@@ -33,7 +35,7 @@ fs.appendFile(
 )
 
 fs.appendFile(
-  'generated.md',
+  READEME_FILENAME,
   String.prototype.concat(H3(`Classes`), Br, Br),
   function (err) {
     if (err) throw err
@@ -42,11 +44,11 @@ fs.appendFile(
 
 // Classes
 
-fs.readdir(layout, (err, files) => {
+fs.readdir(components, (err, files) => {
   const elementClasses = []
 
   fs.appendFile(
-    'generated.md',
+    READEME_FILENAME,
     String.prototype.concat(H4(`Layout`), Br, Br),
     function (err) {
       if (err) throw err
@@ -63,10 +65,89 @@ fs.readdir(layout, (err, files) => {
   })
 
   elementClasses.forEach((elementClass) => {
-    fs.appendFile('generated.md', `- .${elementClass}\r\n`, function (err) {
+    fs.appendFile(READEME_FILENAME, `- .${elementClass}\r\n`, function (err) {
       if (err) throw err
     })
   })
 })
 
-// Variables
+fs.readdir(layout, (err, files) => {
+  const elementClasses = []
+
+  fs.appendFile(
+    READEME_FILENAME,
+    String.prototype.concat(H4(`Layout`), Br, Br),
+    function (err) {
+      if (err) throw err
+    }
+  )
+
+  files.forEach((file) => {
+    const myClass = file.split('.')[0]
+
+    // Don't push index file
+    if (myClass !== 'index') {
+      elementClasses.push(myClass)
+    }
+  })
+
+  elementClasses.forEach((elementClass) => {
+    fs.appendFile(READEME_FILENAME, `- .${elementClass}\r\n`, function (err) {
+      if (err) throw err
+    })
+  })
+})
+
+fs.readdir(elements, (err, files) => {
+  const elementClasses = []
+
+  fs.appendFile(
+    READEME_FILENAME,
+    String.prototype.concat(H4(`Layout`), Br, Br),
+    function (err) {
+      if (err) throw err
+    }
+  )
+
+  files.forEach((file) => {
+    const myClass = file.split('.')[0]
+
+    // Don't push index file
+    if (myClass !== 'index') {
+      elementClasses.push(myClass)
+    }
+  })
+
+  elementClasses.forEach((elementClass) => {
+    fs.appendFile(READEME_FILENAME, `- .${elementClass}\r\n`, function (err) {
+      if (err) throw err
+    })
+  })
+})
+
+fs.readdir(layout, (err, files) => {
+  const elementClasses = []
+
+  fs.appendFile(
+    READEME_FILENAME,
+    String.prototype.concat(H4(`Layout`), Br, Br),
+    function (err) {
+      if (err) throw err
+    }
+  )
+
+  files.forEach((file) => {
+    const myClass = file.split('.')[0]
+
+    // Don't push index file
+    if (myClass !== 'index') {
+      elementClasses.push(myClass)
+    }
+  })
+
+  elementClasses.forEach((elementClass) => {
+    fs.appendFile(READEME_FILENAME, `- .${elementClass}\r\n`, function (err) {
+      if (err) throw err
+    })
+  })
+})
---
+++ docs/readme.md
@@ -0,0 +1,60 @@
+# Semantic Styles
+
+## Responsive, themed, UI design system
+
+### Classes
+
+- .aside-content-grid
+- .container
+- .content-aside-grid
+- .content-grid
+- .content
+- .image-grid
+- .none
+- .only-fullscreen-padding
+- .only-mobile-padding
+- .padding
+- .page
+- .scroll-margin-top
+
+#### Layout
+
+- .card-heading
+- .card-text
+- .card
+- .nav-link
+
+#### Layout
+
+- .aside-content-grid
+- .center
+- .container
+- .content-aside-grid
+- .content-grid
+- .content
+- .image-grid
+- .none
+- .only-fullscreen-padding
+- .only-mobile-padding
+- .padding
+- .page
+- .scroll-margin-top
+
+#### Layout
+
+- .aside
+- .br
+- .btn-primary
+- .btn
+- .fieldset
+- .figcaption
+- .figure
+- .footer
+- .form
+- .header
+- .hr
+- .input
+- .main
+- .nav
+- .textarea
+- .summary
