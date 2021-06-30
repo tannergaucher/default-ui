bad8084aacfa9854a95d54efc520c3c118ceee1d
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2021-06-30 16:42:11 -0400

    Add display none to dialog el on unsupported browsers

--- site/index.js
+++ site/index.js
@@ -3,6 +3,10 @@ const exampleDialog = document.querySelector('#dialog-example')
 
 exampleDialogBtn.addEventListener('click', handleDialogBtnClick)
 
+if (exampleDialog.showModal !== 'function') {
+  exampleDialog.style.display = 'none'
+}
+
 function handleDialogBtnClick() {
   if (typeof exampleDialog.showModal === 'function') {
     exampleDialog.showModal()
