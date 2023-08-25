19c6800d14bed41323aef4b501d3956028cccf76
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-08-25 15:42:25 -0400

    handle closing details menu on click outside

---
+++ package/js/details-menu.js
@@ -0,0 +1,7 @@
+const detailsMenu = document.querySelector('#details-menu')
+
+document.addEventListener('click', function (e) {
+  if (!detailsMenu.contains(e.target)) {
+    detailsMenu.removeAttribute('open')
+  }
+})
