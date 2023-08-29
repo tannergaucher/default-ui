b3af5da2ee84d3910685fa51e8534e5e10ff731f
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-08-29 19:23:41 -0400

    Create Code.stories.js

---
+++ stories/Code.stories.js
@@ -0,0 +1,15 @@
+import '../index.css'
+
+export default {
+  title: 'Card',
+}
+
+export const Default = {
+  render() {
+    const code = document.createElement('code')
+
+    code.innerText = 'npm i semantic-styles'
+
+    return code
+  },
+}
