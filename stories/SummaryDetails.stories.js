b9b400f8e2c83171c2ad7d880a2135fd5f58e776
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-08-29 18:42:22 -0400

    add summary details

---
+++ stories/SummaryDetails.stories.js
@@ -0,0 +1,22 @@
+import '../index.css'
+
+export default {
+  title: 'Summary Details',
+}
+
+export const Primary = () => {
+  const summary = document.createElement('summary')
+  summary.innerText = 'Summary'
+
+  const details = document.createElement('details')
+  details.appendChild(summary)
+
+  const p = document.createElement('p')
+
+  p.innerText =
+    'Details go here. You can put anything here you want. Click the summary to close.'
+
+  details.appendChild(p)
+
+  return details
+}
