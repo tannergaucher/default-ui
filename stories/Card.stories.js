95ec7bb65aaebaff4b578547ed8ce39695a55175
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-08-29 18:29:17 -0400

    Add initil styles

---
+++ stories/Card.stories.js
@@ -0,0 +1,31 @@
+import '../index.css'
+
+export default {
+  title: 'Card',
+}
+
+export const Default = {
+  render() {
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const cardHeading = document.createElement('h3')
+    cardHeading.classList.add('card-heading')
+    cardHeading.innerText = 'Card Header'
+
+    const cardImage = document.createElement('img')
+    cardImage.classList.add('card-image')
+    // random image from unsplash
+    cardImage.src = 'https://source.unsplash.com/random/300x200'
+
+    const cardText = document.createElement('p')
+    cardText.classList.add('card-text')
+    cardText.innerText = 'This is card text'
+
+    card.appendChild(cardImage)
+    card.appendChild(cardHeading)
+    card.appendChild(cardText)
+
+    return card
+  },
+}
