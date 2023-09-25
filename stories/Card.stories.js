7bb454f80006ba3dd8b2c1aaea6cda95a97ffeae
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-09-25 17:51:16 -0400

    Add tunnel script

--- stories/Card.stories.js
+++ stories/Card.stories.js
@@ -1,30 +1,12 @@
 import '../index.css'
 
+import { Card } from './Card'
+
 export default {
+  component: Card,
   title: 'Card',
 }
 
 export const Default = {
-  render() {
-    const card = document.createElement('div')
-    card.classList.add('card')
-
-    const cardHeading = document.createElement('h3')
-    cardHeading.classList.add('card-heading')
-    cardHeading.innerText = 'Card Header'
-
-    const cardImage = document.createElement('img')
-    cardImage.classList.add('card-image')
-    cardImage.src = 'https://source.unsplash.com/random/300x200'
-
-    const cardText = document.createElement('p')
-    cardText.classList.add('card-text')
-    cardText.innerText = 'This is card text'
-
-    card.appendChild(cardImage)
-    card.appendChild(cardHeading)
-    card.appendChild(cardText)
-
-    return card
-  },
+  args: {},
 }
