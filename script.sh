8a7d4fccb17f329139901a30e22b29b983fbbdd9
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-09-30 14:17:00 -0400

    fix

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@ CHANGESET_FILE=$(find .changeset -name '*.md' ! -name README.md | head -n 1)
 CHANGESET_LINES=$(cat "$CHANGESET_FILE")
 
 
-
 MAJOR=$(grep -o '"semantic-styles": major' <<< "$CHANGESET_LINES")
 MINOR=$(grep -o '"semantic-styles": minor' <<< "$CHANGESET_LINES")
 PATCH=$(grep -o '"semantic-styles": patch' <<< "$CHANGESET_LINES")
