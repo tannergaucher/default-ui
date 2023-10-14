2ac69919bad93d8e62438529ca6e3461826b06ab
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-10-14 09:25:41 -0400

    update accent color vars, site

--- site/index.ts
+++ site/index.ts
@@ -6,23 +6,3 @@ import { useTheme } from '../package/ts/theme'
 
 useProgressiveEnhancements()
 useTheme()
-
-const accent1Picker =
-  document.querySelector<HTMLInputElement>('#accent-1-input')
-
-accent1Picker?.addEventListener('input', (event: any) => {
-  const accent1 = event.target.value
-  document.documentElement.style.setProperty('--accent-1', accent1)
-
-  //   also persist to local storage
-
-  //   const currentMode = localStorage.getItem(THEME_STORAGE_KEY) as Mode
-})
-
-const accent2Picker =
-  document.querySelector<HTMLInputElement>('#accent-2-input')
-
-accent2Picker?.addEventListener('input', (event: any) => {
-  const accent2 = event.target.value
-  document.documentElement.style.setProperty('--accent-2', accent2)
-})
