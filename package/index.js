32ee2406fe944899365edb54d2acaa95c9be2251
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-10-16 09:17:09 -0400

    fix imports, fix handling storage mode

--- package/index.js
+++ package/index.js
@@ -1,7 +1,6 @@
-import './elements/index.css'
-import './variables/index.css'
-
 import { useTheme } from './ts/theme'
 import { useProgressiveEnhancements } from './ts/progressive-enhancements'
+
+import './index.css'
 
 export { useTheme, useProgressiveEnhancements }
