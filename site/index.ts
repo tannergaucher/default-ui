c60554970fc1c9d2726bd1784f548a4a0233d981
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-10-18 08:02:31 -0400

    Update index.ts

--- site/index.ts
+++ site/index.ts
@@ -1,9 +1,6 @@
-// import package styles
 import '../package/index.css'
-// import local styles
 import './local.css'
 
-// import package theme and progressive enhancements
 import { useTheme, useProgressiveEnhancements } from '../package/index.js'
 
 useTheme()
